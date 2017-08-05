729c0921dd0cd156bf2f69f9182e0556f32f0179
Nicholas Feldman <nickcfeldman@gmail.com>
2017-08-05 14:12:10 -0400

    Create main class and README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Nude.kt
+
+Kotlin Nudity Detection
+
+This is a port of [nude.js](https://github.com/pa7/nude.js)
+to [Kotlin](https://kotlinlang.org/).
---
+++ src/main/kotlin/tech/feldman/nudekt/Nude.kt
@@ -0,0 +1,13 @@
+package tech.feldman.nudekt
+
+import java.awt.image.BufferedImage
+import java.io.File
+import javax.imageio.ImageIO
+
+fun isNude(path: String) = isNude(File(path))
+
+fun isNude(file: File) = isNude(ImageIO.read(file))
+
+fun isNude(image: BufferedImage): Boolean {
+    return false
+}
